d18755f383ea09356f8695e2a1d77d03303de36e
DerNikJakom <jan.komnik@googlemail.com>
2024-07-16 12:31:49 +0200

    try Card Component at default case

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import InventoryTable from "./InventoryTable";
 import MenuButtons from "./MenuButtons";
 import BackButton from "./BackButton";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
 
 export default function Home(props) {
   const [request, setRequest] = useState("");
@@ -47,12 +50,17 @@ export default function Home(props) {
 
     default:
       console.log(`Case: Default`);
+      // TODO: Card Design wie in Figma
       return (
-        <>
-          <h1>ASKUMA Manager</h1>
-          <h2>Was möchtest du tun?</h2>
-          <MenuButtons userInput={setRequest} />
-        </>
+        <Card>
+          <CardContent>
+            <h1>ASKUMA Manager</h1>
+            <h2>Was möchtest du tun?</h2>
+            <CardActions>
+              <MenuButtons userInput={setRequest} />
+            </CardActions>
+          </CardContent>
+        </Card>
       );
   }
 }
