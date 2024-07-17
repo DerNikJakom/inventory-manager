88b3b6c4cf276ecd2e6e4e30f35df603c3723864
DerNikJakom <jan.komnik@googlemail.com>
2024-07-17 13:37:09 +0200

    delete functions no longer needed

--- src/components/InventoryTable.jsx
+++ src/components/InventoryTable.jsx
@@ -41,15 +41,6 @@ export default function InventoryTable(props) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
   return (
     <Card sx={{ borderRadius: 3, backgroundColor: "#F2F7F8" }}>
       <CardContent>
