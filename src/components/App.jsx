6746cbc66693b51ae4e611736107518cb36d96cb
DerNikJakom <jan.komnik@googlemail.com>
2024-08-22 13:49:43 +0200

    create router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,50 @@
 import React, { useState } from "react";
-import SignIn from "./SignIn";
-import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import SignIn from "./SignIn";
+// import Home from "./Home";
 import "../styles/App.css";
+import Homepage from "../pages/Homepage";
+import NoPage from "../pages/NoPage";
+import Layout from "../pages/Layout";
+import LogIn from "../pages/LogIn";
+import SignUp from "../pages/SignUp";
 
 export default function App() {
-  const [isRegistered, setRegistered] = useState(false);
-  const [isLoggedIn, setLoggedIn] = useState(false); // ! Status richtig setzen, evtl auslagern
-  const [currentUserID, setCurrentUserID] = useState(0);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="login" element={<LogIn />} />
+          <Route path="register" element={<SignUp />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 
-  function logOut() {
-    setLoggedIn(false);
-  }
-
-  if (isLoggedIn) {
-    return (
-      <>
-        <Home logOut={logOut} userID={currentUserID} />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <SignIn
-          login={setLoggedIn}
-          isRegistered={isRegistered}
-          setRegistered={setRegistered}
-          setUserID={setCurrentUserID}
-        />
-      </>
-    );
-  }
+  // const [isRegistered, setRegistered] = useState(false);
+  // const [isLoggedIn, setLoggedIn] = useState(false);
+  // const [currentUserID, setCurrentUserID] = useState(0);
+  // function logOut() {
+  //   setLoggedIn(false);
+  // }
+  // if (isLoggedIn) {
+  //   return (
+  //     <>
+  //       <Home logOut={logOut} userID={currentUserID} />
+  //     </>
+  //   );
+  // } else {
+  //   return (
+  //     <>
+  //       <SignIn
+  //         login={setLoggedIn}
+  //         isRegistered={isRegistered}
+  //         setRegistered={setRegistered}
+  //         setUserID={setCurrentUserID}
+  //       />
+  //     </>
+  //   );
+  // }
 }
